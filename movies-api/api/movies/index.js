799930ba847767cfbe3697839105eac7880be9a9
Yiming Yu <2693604213@qq.com>
2023-12-21 21:16:49 +0000

    Upcoming movies from TMDB

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,3 +1,6 @@
+import {
+    getUpcomingMovies
+  } from '../tmdb-api';
 import movieModel from './movieModel';
 import asyncHandler from 'express-async-handler';
 import express from 'express';
@@ -18,6 +21,10 @@ router.get('/:id', asyncHandler(async (req, res) => {
     } else {
         res.status(404).json({message: 'The movie you requested could not be found.', status_code: 404});
     }
+}));
+router.get('/tmdb/upcoming', asyncHandler(async (req, res) => {
+    const upcomingMovies = await getUpcomingMovies();
+    res.status(200).json(upcomingMovies);
 }));
 
 export default router;
--- movies-api/index.js
+++ movies-api/index.js
@@ -1,3 +1,4 @@
+import moviesRouter from './api/movies';   //import movies router
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
@@ -14,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/users', usersRouter);
 app.use(defaultErrHandler);
-
+app.use('/api/movies', moviesRouter); //ADD THIS BEFORE THE DEFAULT ERROR HANDLER.
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
 });
