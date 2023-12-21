ec4f524c7638c47eab1abc8aeba5e63ff8babf7f
Yiming Yu <2693604213@qq.com>
2023-12-21 21:41:55 +0000

    Salt and Hash

--- movies-api/api/users/userModel.js
+++ movies-api/api/users/userModel.js
@@ -1,5 +1,5 @@
+import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
-
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
@@ -7,4 +7,28 @@ const UserSchema = new Schema({
   password: {type: String, required: true }
 });
 
+UserSchema.methods.comparePassword = async function (passw) { 
+  return await bcrypt.compare(passw, this.password); 
+};
+
+UserSchema.statics.findByUserName = function (username) {
+  return this.findOne({ username: username });
+};
+
+UserSchema.pre('save', async function(next) {
+  const saltRounds = 10; // You can adjust the number of salt rounds
+  //const user = this;
+  if (this.isModified('password') || this.isNew) {
+    try {
+      const hash = await bcrypt.hash(this.password, saltRounds);
+      this.password = hash;
+      next();
+  } catch (error) {
+     next(error);
+  }
+
+  } else {
+      next();
+  }
+});
 export default mongoose.model('User', UserSchema);
