e283323867c873ae700c8a3427db319ae6a0db0d
Yiming Yu <2693604213@qq.com>
2023-12-22 04:18:52 +0000

    Topic 12 - Get Movies from API

--- reactApp/src/api/movies-api.js
+++ reactApp/src/api/movies-api.js
@@ -1,9 +1,14 @@
 export const getMovies = async () => {
-    const response = await  fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=49646098df4cff8b6bf7e0407ba8490c&language=en-US&include_adult=false&page=1`
-    )
-    return response.json()
-  };
+  const response = await fetch(
+    'http://localhost:8080/api/movies', {
+    headers: {
+      'Authorization': window.localStorage.getItem('token')
+    }
+  }
+  )
+  return response.json();
+};
+
 
   export const login = async (username, password) => {
     const response = await fetch('http://localhost:8080/api/users', {
